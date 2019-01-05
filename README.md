2edf5fa325c82fb59c5e576be44d197e8a5301db
Taher <taher.dasten@gmail.com>
2019-01-06 00:45:16 +0530

    iniital commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## gatsby-remark-video
+
+Embeds video tag
+
+## Installation
+```
+npm install gatsby-remark-video
+```
+
+## Usage
+
+In your markdown
+```
+video: /static/shots-demo-369bfe714a6b8981ecfc743f7e7e7008.mp4
+```
+
+Add the following in your `gatsby-config.js`
+```javascript
+{
+	resolve: 'gatsby-remark-video',
+	options: {
+		width: 800,
+		height: 'auto',
+		preload: 'auto',
+		muted: true,
+		autoplay: true
+	}
+}
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+const visit = require('unist-util-visit');
+
+const addVideo = ({ markdownAST }, options) => {
+
+	visit(markdownAST, 'inlineCode', (node) => {
+		const { value } = node;
+		const matches = value.match(/video:?\s(.*)+/i);
+
+		if(matches) {
+			const url = matches[1].trim();
+
+			node.type = 'html';
+			node.value = renderVideoTag(url, options);
+		}
+	});
+
+};
+
+const renderVideoTag = (url, options) => {
+
+	const videoNode = `
+		<video
+			src=${url}
+			width="${options.width}"
+			height="${options.height}"
+			preload="${options.preload}"
+			muted="${options.muted}"
+			${options.autoplay ? 'autoplay' : ''}
+		></video>
+	`;
+
+	return videoNode;
+};
+
+module.exports = addVideo;
