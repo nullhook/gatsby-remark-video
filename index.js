a39262a82b4893d743f3b2904f4bb624212bf23e
Nathaniel Nunes <manutdkid77@gmail.com>
2019-10-07 00:38:38 +0530

    add helper method to get mime type

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
 const visit = require('unist-util-visit');
 
 const addVideo = ({ markdownAST }, options) => {
-
+  
 	visit(markdownAST, 'inlineCode', (node) => {
-		const { value } = node;
-		const matches = value.match(/video:?\s(.*)+/i);
+    const { value } = node;
+    const matches = value.match(/video:?\s(.*)+/i);
 
-		if(matches) {
-			const url = matches[1].trim();
+    if(matches) {
+      const url = matches[1].trim();
 
-			node.type = 'html';
-			node.value = renderVideoTag(url, options);
-		}
-	});
+      node.type = 'html';
+      node.value = renderVideoTag(url, options);
+    }
+  });
 
 };
 
 const renderVideoTag = (url, options) => {
 
-	const videoNode = `
+  const videoNode = `
 		<video
 			src=${url}
 			width="${options.width}"
@@ -31,7 +31,22 @@ const renderVideoTag = (url, options) => {
 		></video>
 	`;
 
-	return videoNode;
+  return videoNode;
+};
+
+const getMimeType = extension => {
+  switch (extension) {
+    case 'mp4':
+      return 'mp4';
+    case 'ogg':
+      return 'ogg';
+    case 'ogv':
+      return 'ogg';
+    case 'webm':
+      return 'webm';
+    default:
+      return '';
+  }
 };
 
 module.exports = addVideo;
